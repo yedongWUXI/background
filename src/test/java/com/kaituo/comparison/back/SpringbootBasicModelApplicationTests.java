bcd97f59e146cacaad79341757a956a60477b35c
yedong <zhyd704@126.com>
2019-04-21 23:03:04 +0800

    嵌入xxl配置中心

--- src/test/java/com/kaituo/comparison/back/SpringbootBasicModelApplicationTests.java
+++ src/test/java/com/kaituo/comparison/back/SpringbootBasicModelApplicationTests.java
@@ -1,16 +1,37 @@
 package com.kaituo.comparison.back;
 
-import org.junit.Test;
+import com.kaituo.comparison.back.core.config.configcenter.ConfigResource;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
+@RestController
 @SpringBootTest
 public class SpringbootBasicModelApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Resource
+    ConfigResource configResource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasicModelApplicationTests.class, args);
+    }
+
+    //测试配置、热加载
+    @RequestMapping("test")
+    public String test(){
+        return configResource.redisIp+configResource.redisPort;
+    }
+
+    //测试是否通
+    @RequestMapping("test1")
+    public String test1(){
+        return "success";
     }
 
 }
